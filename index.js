7ff7b885d90eb94acc334ddea3f1433a7c766506
Alberto Lopez <102048476+AlbertoJL3@users.noreply.github.com>
2023-01-10 08:45:05 -0400

    Optimized function

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 function spreadthewealth(arr) {
-
-var sum = 0;
-
-//sum values of the array
-for(var i = 0; i < arr.length; i++) {
-    sum += arr[i];
-}
-
-//get the remainder of the sum 
-const remainder = sum / arr.length; 
-
-//subtract remainder from the sum
-sum -= remainder
-
-//get average integer of the array
-avg = sum/arr.length;
-var newArr = [];
-
-//replace the array elements by the average 
-for (var j = 0; j<arr.length; j++) {
-newArr[j] = avg;
-}
-
- if (remainder !== 0) {
+    const sum = arr.reduce((total, current) => total + current);
+    const remainder = sum % arr.length;
+    sum = sum - remainder
+    const avg = sum / arr.length;
+    const newArr = arr.map(() => avg);
+    if (remainder !== 0) {
         newArr.push(remainder);
     }
-return newArr
+    return newArr;
 }
